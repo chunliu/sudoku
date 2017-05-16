fef8054b6bbdad81b86a635765555ac81f08605a
Chun Liu <chunliu@users.noreply.github.com>
2017-05-16 16:30:37 +0800

    remove unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -61,22 +61,10 @@ module.exports = {
                     {loader: 'awesome-typescript-loader'}, 
                 ] 
             },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-            // { test: /\.css$/, loader: "style-loader!css-loader" },
-            // { test: /\.png$/, loader: "url-loader?limit=100000" },
-            // { test: /\.jpg$/, loader: "file-loader" },
-            // { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-            // { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-            // { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
-            // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },                       
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },                  
         ]
     },
-    plugins: [
-        // new webpack.DefinePlugin({
-        //     'process.env': {
-        //         'NODE_ENV': JSON.stringify('production')
-        //     }
-        // }),        
+    plugins: [     
         new webpack.HotModuleReplacementPlugin(),
         // enable HMR globally
         new webpack.NamedModulesPlugin(),
